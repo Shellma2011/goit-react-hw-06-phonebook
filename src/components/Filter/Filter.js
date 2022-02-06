4b1b6314dec752b65ad45646c7c557afd3df1ac6
Maryna Shevchenko <Shellma2011@gmail.com>
2022-02-06 21:04:35 +0200

    moved styles to separate components

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,32 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from 'styled-components';
-
-const FilterLabel = styled.label`
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-
-  width: 300px;
-  margin: 0 auto;
-
-  padding: 12px 16px;
-  border: 1px solid #2a2a2a;
-  border-radius: 4px;
-
-  font: inherit;
-  line-height: 16px;
-  letter-spacing: 0.01em;
-`;
-
-const FilterInput = styled.input`
-  padding: 4px 12px;
-  width: 270px;
-  margin-top: 5px;
-  font: inherit;
-  line-height: 12px;
-  letter-spacing: 0.01em;
-`;
+import { FilterLabel, FilterInput } from './Filter.styled';
 
 const Filter = ({ value, onChange }) => (
   <FilterLabel>
